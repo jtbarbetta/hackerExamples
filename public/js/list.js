9c9e956ef656ca0550ef3deb271c158baa8e54d4
Jackie Barbetta <jackieb@us.ibm.com>
2017-05-15 18:20:36 -0400

    Just a little cleanup

--- public/js/list.js
+++ public/js/list.js
@@ -5,6 +5,7 @@
             $scope.itemList = [];
             
             $scope.addListItem = function(){
+                // keeps controller independent of view and therefore easier to test
                 if ($scope.newItem && $scope.newItem != "") {
                     var css = (($scope.itemList.length+1) % 3) === 0 ? 'red-item' : 'black-item';
                     $scope.itemList.push({name: $scope.newItem, css: css});
